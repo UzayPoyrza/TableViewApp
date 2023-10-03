477786e596ee1bfc8d0c9fd734085451e5b14598
uzay-optimum7 <uzay@optimum7.com>
2023-10-03 12:53:50 -0400

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Uzay Poyraz on 3.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
