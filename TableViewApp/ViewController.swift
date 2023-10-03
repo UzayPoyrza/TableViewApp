756996abe375cd7152dc92541f1f7fb97e4cadf9
uzay-optimum7 <uzay@optimum7.com>
2023-10-03 13:49:43 -0400

    Table View of Skidmore Messaging App

--- TableViewApp/ViewController.swift
+++ TableViewApp/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    let sections = ["Courses", "ResLife", "Direct Messages", "Clubs", "Health", "Settings", "Switch to Icon View"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print("you tapped me!")
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = sections[indexPath.row]
+        
+        return cell
+    }
+}
